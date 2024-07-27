d70c7417f8b85a2a92d16535dd4064a616d62fe7
i12laroa <63612440+i12laroa@users.noreply.github.com>
2024-07-27 16:29:32 +0200

    Agregar archivos de proyecto.

---
+++ UD1-Ejemplo1ConsolaArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD1_Ejemplo1ConsolaArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vamos a crear un array con 3 palabras
+            string[] palabras = new string[3];
+            int contador = 0;
+
+            //Realizamos un bucle para contar las palabras
+            while (contador != 3)
+            {
+                Console.WriteLine($"Introduce una palabra en nuestro array (Posición {contador+1}):");
+                palabras[contador] = Console.ReadLine();
+                contador++;
+            }
+
+            //Mostramos el array introducido
+            foreach(string palabra in palabras) { Console.WriteLine(palabra); }
+
+        }
+    }
+}
